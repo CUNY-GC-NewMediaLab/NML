8e31398fca656c4f9bf7693a98d4031d15a68747
Laura Henze <laurahenze@gmail.com>
2013-08-27 14:57:56 -0400

    Workaround for a Chrome list item float list-style-type bug.

--- js/nml.js
+++ js/nml.js
@@ -1,16 +1,18 @@
 $(function() {
- console.log("Jquery yay");
-    $(".sortbyOptions").change(function() {
-    console.log("changed");
-        $('.headerLi').remove();
+ 
+    $("#sorter1").click(function() {
+     	$('.headerLi').remove();
         $('ul#peopleList1>li').removeClass("floatme");
-        var whichVal = $(this).val();
-        if (whichVal == 'name') {
+        if ($(this).data('sortcriteria') === 'name') {
+        	$(this).data('sortcriteria', 'department');
+        	$(this).html("View by doctoral program");
             $('ul#peopleList1>li').addClass("floatme");
             $('ul#peopleList1>li').tsort({
                 data: 'rank'
             });
         } else {
+        	$(this).data('sortcriteria', 'name');
+        	$(this).html("View by name");
             var dept, oldDept;
             $('ul#peopleList1>li').tsort({
                 data: 'department'
@@ -24,17 +26,19 @@ $(function() {
             });
         }
     });
-    $(".sortbyOptions2").change(function() { 
-    	console.log("change");
+	$("#sorter2").click(function() {    	
         $('.headerLi2').remove();
         $('ul#peopleList2>li').removeClass("floatme");
-        var whichVal = $(this).val();
-        if (whichVal == 'name') {
+        if ($(this).data('sortcriteria') === 'name') {	
+        	$(this).data('sortcriteria', 'department');
+        	$(this).html("View by doctoral program");
             $('ul#peopleList2>li').addClass("floatme");
             $('ul#peopleList2>li').tsort({
                 data: 'rank'
             });
         } else {
+        	$(this).data('sortcriteria', 'name');
+        	$(this).html("View by name");
             var dept, oldDept;
             $('ul#peopleList2>li').tsort({
                 data: 'department'
