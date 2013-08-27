cfd8ca77df0e3d1c90260bc1bebe136b2ff59e70
Laura Henze <laurahenze@gmail.com>
2013-08-27 11:19:36 -0400

    updated to HTML5.  New UI for sorting mechanism.

--- js/nml.js
+++ js/nml.js
@@ -1,5 +1,7 @@
 $(function() {
+ console.log("Jquery yay");
     $(".sortbyOptions").change(function() {
+    console.log("changed");
         $('.headerLi').remove();
         $('ul#peopleList1>li').removeClass("floatme");
         var whichVal = $(this).val();
@@ -17,6 +19,30 @@ $(function() {
                 dept = $El.data('department');
                 if (oldDept !== dept) {
                     $El.before("<li class='headerLi'>" + dept + "</li>");
+                }
+                oldDept = dept;
+            });
+        }
+    });
+    $(".sortbyOptions2").change(function() { 
+    	console.log("change");
+        $('.headerLi2').remove();
+        $('ul#peopleList2>li').removeClass("floatme");
+        var whichVal = $(this).val();
+        if (whichVal == 'name') {
+            $('ul#peopleList2>li').addClass("floatme");
+            $('ul#peopleList2>li').tsort({
+                data: 'rank'
+            });
+        } else {
+            var dept, oldDept;
+            $('ul#peopleList2>li').tsort({
+                data: 'department'
+            }).each(function(i, el) {
+                var $El = $(el);
+                dept = $El.data('department');
+                if (oldDept !== dept) {
+                    $El.before("<li class='headerLi2'>" + dept + "</li>");
                 }
                 oldDept = dept;
             });
--- peopleprojectpage.php
+++ peopleprojectpage.php
@@ -4,51 +4,45 @@ Template Name: People Root Page
 */
 ?>
 <?php get_header() ?>
+
 <div id="container">
-	<div id="blogcontent">
-<?php the_post() ?>
-		<div id="post-<?php the_ID() ?>" class="<?php sandbox_post_class() ?>">
-			<h2 class="entry-title">
-<?php the_title() ?>
-			</h2>
-			<div class="entry-content peoplepage">
-<?php the_content() ?>
-				<div class="projectborder">
-					<h2 class="entry-title">
-						Managing Director
-					</h2>
-					<ul class="people">
-<?php wp_list_pages( "depth=1&title_li=&sort_column=menu_order&child_of=507" ); // display the sub pages of the current page only ?>
-					</ul>
-				</div>
-				<div class="projectborder">
-					<h2 class="entry-title">
-						Project Advisors
-					</h2>
-					<ul class="people">
-<?php wp_list_pages( "depth=1&title_li=&sort_column=menu_order&child_of=513" ); // display the sub pages of the current page only ?>
-					</ul>
-				</div>
-				<div class="projectborder">
-					<h2 class="entry-title">Co-Directors</h2>
-<ul class="people">
-<?php wp_list_pages( "depth=1&title_li=&sort_column=menu_order&child_of=509" ); // display the sub pages of the current page only ?>
-</ul>
-				</div>
-				<div class="projectborder">
-					<select class="sortbyOptions" style="float:right; margin-bottom:-15px">
-						<option id="byAlpha1"  value="name" >View by name</li>
-						<option id="byDept1"  value="data-department" >View by department</li>
-					</select>
-					<div class="chooseToggle">
-					<a id="byAlpha1"  data-value="name" >viewing by name</a> |</li>
-						<a id="byDept1"  data-value="data-department" >view by department</a></li>
-					</div>
-	<h2 class="entry-title">Graduate Students and Faculty</h2>
-<ul id = "peopleList1">
-<?php
+  <div id="blogcontent">
+    <?php the_post() ?>
+    <div id="post-<?php the_ID() ?>" class="<?php sandbox_post_class() ?>">
+      <h2 class="entry-title">
+        <?php the_title() ?>
+      </h2>
+      <div class="entry-content peoplepage">
+        <?php the_content() ?>
+        <div class="projectborder">
+          <h2 class="entry-title"> Managing Director </h2>
+          <ul class="people">
+            <?php wp_list_pages( "depth=1&title_li=&sort_column=menu_order&child_of=507" ); // display the sub pages of the current page only ?>
+          </ul>
+        </div>
+        <div class="projectborder">
+          <h2 class="entry-title"> Project Advisors </h2>
+          <ul class="people">
+            <?php wp_list_pages( "depth=1&title_li=&sort_column=menu_order&child_of=513" ); // display the sub pages of the current page only ?>
+          </ul>
+        </div>
+        <div class="projectborder">
+          <h2 class="entry-title">Co-Directors</h2>
+          <ul class="people">
+            <?php wp_list_pages( "depth=1&title_li=&sort_column=menu_order&child_of=509" ); // display the sub pages of the current page only ?>
+          </ul>
+        </div>
+        <div class="projectborder">
+          <select class="sortbyOptions" style="float:right; margin-bottom:-15px">
+            <option id="byAlpha1"  value="name" >View by name
+            </li>
+            <option id="byDept1"  value="data-department" >View by department
+            </li>
+          </select>
+          <h2 class="entry-title">Graduate Students and Faculty</h2>
+          <ul id = "peopleList1">
+            <?php
 $pages = get_pages( 'echo=0&depth=1&title_li=&sort_column=menu_order&child_of=515' );
-//$page_col = round( count($pages ) / 2 );
 foreach ( $pages as $i=>$page ) {
 	
 	$departmentTaxo = wp_get_object_terms( $page->ID, 'departments' );
@@ -64,12 +58,7 @@ foreach ( $pages as $i=>$page ) {
 		$myDept = $x[0] ;
 	}
 	$dept = ( isset( $myDept ) ) ? $myDept : '';
-	//if ( $i<$page_col ) {
-		$option .= '<li  class="floatme" data-department = "' . $dept . '" data-rank= "' . $i . '">';
-	//} else {
-	//	$option .= '<li  class="pageright"  data-department = "' . $dept . '" data-rank= "' . $i . '">';
-	//}
-	//$option .= '<li  data-department = "' . $dept . '" data-rank= "' . $i . '">';
+	$option .= '<li  class="floatme" data-department = "' . $dept . '" data-rank= "' . $i . '">';
 	$option .= '<a href="' . get_page_link( $page->ID ) . '">';
 	$option .= $page->post_title;
 	$option .= '</a></li>';
@@ -78,51 +67,58 @@ foreach ( $pages as $i=>$page ) {
 	echo $option;
 }
 ?>
- </ul>
-
-				</div>
-				<div class="projectborder">
-					<h2 class="entry-title">
-						Visiting Researchers
-					</h2>
-					<ul class="people">
-					<?php wp_list_pages( 'depth=1&title_li=&sort_column=menu_order&child_of=2000' ); // display the sub pages of the current page only ?>
-					</ul>
-				</div>
-				<div class="projectborder">
-					<h2 class="entry-title">
-						Past Researchers
-					</h2>
-						<?php
-$page_s = explode( "</li>", wp_list_pages( 'echo=0&depth=1&title_li=&sort_column=menu_order&child_of=597' ) );
-$page_n = count( $page_s ) - 1;
-$page_col = round( $page_n / 2 );
-for ( $i=0;$i<$page_n;$i++ ) {
-	if ( $i<$page_col ) {
-		$page_left = $page_left.''.$page_s[$i].'</li>';
-	} elseif ( $i>=$page_col ) {
-		$page_right = $page_right.''.$page_s[$i].'</li>';
-	}
-}
+          </ul>
+        </div>
+        <div class="projectborder">
+          <h2 class="entry-title"> Visiting Researchers </h2>
+          <ul class="people">
+            <?php wp_list_pages( 'depth=1&title_li=&sort_column=menu_order&child_of=2000' ); // display the sub pages of the current page only ?>
+          </ul>
+        </div>
+        <div class="projectborder">
+          <select class="sortbyOptions2" style="float:right; margin-bottom:-15px">
+            <option id="byAlpha2"  value="name" >View by name
+            </li>
+            <option id="byDept2"  value="data-department" >View by department
+            </li>
+          </select>
+          <h2 class="entry-title"> Past Researchers </h2>
+          <ul id = "peopleList2">
+            <?php
+		$pages = get_pages( 'echo=0&depth=1&title_li=&sort_column=menu_order&child_of=597' );
+		foreach ( $pages as $i=>$page ) {
+		$departmentTaxo = wp_get_object_terms( $page->ID, 'departments' );
+		if ( !empty( $departmentTaxo ) ) {
+			if ( !is_wp_error( $departmentTaxo ) ) {
+				$myDept =  $departmentTaxo[0] ->name ;
+			}
+		}
+		$option = '';
+		if ( !isset( $myDept ) ) {
+			$x =  get_post_meta( $page->ID, 'stdepartment', false );
+			wp_set_post_terms(  $page->ID, $x[0] , 'departments' );
+			$myDept = $x[0] ;
+		}
+		$dept = ( isset( $myDept ) ) ? $myDept : '';
+		$option .= '<li  class="floatme" data-department = "' . $dept . '" data-rank= "' . $i . '">';
+		$option .= '<a href="' . get_page_link( $page->ID ) . '">';
+		$option .= $page->post_title;
+		$option .= '</a></li>';
+		unset( $dept );
+		unset( $myDept );
+		echo $option;
+		}
 ?>
-					<ul class="left people">
-<?php echo $page_left; ?>
-					</ul>
-					<ul class="right people">
-<?php echo $page_right; ?>
-					</ul>
-
-
-	</div>
-	<h2 class="entry-title">&nbsp;</h2>
-<?php edit_post_link( __( 'Edit', 'sandbox' ), '<span class="edit-link">', '</span>' ) ?>
-			</div>
-		</div>
-<!-- .post -->
-	</div>
-<!-- #content -->
+          </ul>
+        </div>
+        <h2 class="entry-title">&nbsp;</h2>
+        <?php edit_post_link( __( 'Edit', 'sandbox' ), '<span class="edit-link">', '</span>' ) ?>
+      </div>
+    </div>
+    <!-- .post --> 
+  </div>
+  <!-- #content --> 
 </div>
 <!-- #container -->
-
 
 <?php get_footer() ?>
